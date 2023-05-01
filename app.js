d59cb4249fe8088917666b817312bf46359398e5
Vanya Mark <markova.vanya@gmail.com>
2023-05-01 15:48:42 +0200

    updated app js

--- app.js
+++ app.js
@@ -9,14 +9,22 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+const { isAuthenticated } = require("./middleware/jwt.middleware");
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+/*const indexRoutes = require("./routes/index.routes");
+app.use("/api", indexRoutes); */
+
+const allRoutes = require("./routes");
+app.use("/api", allRoutes);
+
+const authRouter = require("./routes/auth.routes");       //  <== IMPORT
+app.use("/auth", authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
