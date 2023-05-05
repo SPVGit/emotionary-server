b9eb53c20c930716b61b1b11c0a82fea429a58a6
Vanya Mark <markova.vanya@gmail.com>
2023-05-05 18:13:45 +0200

    Working delete post from Post collection but not from User array

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const User = require("../models/User.model")
+const User = require("../models/User.model");
 const Post = require("../models/Post.model");
-
 
 //  POST /api/posts  -  Creates a new post
 router.post("/addpost", (req, res, next) => {
   const { emotion, rating, description, userId } = req.body;
 
   console.log(req.body);
-  
-  Post.create({ emotion, rating, description, user:userId})
-  .then((newPost) => {
-    return User.findByIdAndUpdate(userId, {
-      $push: { posts: newPost._id },
-    });
-  })
+
+  Post.create({ emotion, rating, description, user: userId })
+    .then((newPost) => {
+      return User.findByIdAndUpdate(userId, {
+        $push: { posts: newPost._id },
+      });
+    })
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
 });
@@ -24,12 +23,13 @@ router.post("/addpost", (req, res, next) => {
 //  GET /api/posts -  Retrieves all of the posts
 router.get("/posts", (req, res, next) => {
   Post.find()
-  //  .populate("posts")
+    //  .populate("posts")
     .then((allPosts) => {
-      console.log('allPosts', allPosts)
-      res.json(allPosts)})
+      console.log("allPosts", allPosts);
+      res.json(allPosts);
+    })
     .catch((err) => res.json(err));
-}); 
+});
 
 //  GET /api/posts/:postId -  Retrieves a specific post by id
 router.get("/posts/:postId", (req, res, next) => {
@@ -39,10 +39,41 @@ router.get("/posts/:postId", (req, res, next) => {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
- // Each Post document has `tasks` array holding `_id`s of Task documents
+  // Each Post document has `tasks` array holding `_id`s of Task documents
   // We use .populate() method to get swap the `_id`s for the actual Task documents
   Post.findById(postId)
     .then((post) => res.status(200).json(post))
-    .catch((error) => res.json(error)); 
-})
-module.exports = router
+    .catch((error) => res.json(error));
+});
+
+//DELETE Delete post
+
+router.delete("/posts/:postId", (req, res, next) => {
+  const { postId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(`${postId}`)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+//  Post.deleteOne({_id: ObjectId(postId)})
+//  User.updateMany({}, {$pull: {posts: ObjectId(`${postId}`)}})
+
+   Post.findByIdAndRemove(postId)
+    .then(() =>
+/*  User.find()
+        .then((user)=> {
+          if (user.posts) {
+
+            console.log("user posts", user.posts)
+          } else {console.log('user', user)}
+        })  */
+      res.json({
+        message: `Post with ${postId} is removed successfully.`,
+      })
+   )
+    .catch((error) => res.json(error));
+});
+module.exports = router;
+
+
