c9a963d4919ae830394bcbfd00351782a39d3f61
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-05-11 20:30:59 +0200

    UI and therapist auth

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const express = require("express")
 
 const app = express()
 
-const { isAuthenticated } = require("./middleware/jwt.middleware")
+const { isAuthenticated, isTherapistAuthenticated } = require("./middleware/jwt.middleware")
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app)
-
 
 const authRouter = require("./routes/auth.routes") //  <== IMPORT
 app.use("/auth", authRouter)
@@ -31,6 +30,9 @@ app.use("/", isAuthenticated, indexRouter)
 
 const chatRouter = require("./routes/chat.routes")
 app.use("/", isAuthenticated, chatRouter)
+
+const therapistRouter = require("./routes/therapist.routes")
+app.use("/", therapistRouter)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app)
